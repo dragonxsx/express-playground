3a520fb20d2b1432b0a3f621ec76c36977ee7080
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-10-27 14:24:20 +0700

    Express 101 - Serving static files

--- express101/expressServer.js
+++ express101/expressServer.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/home.html'));
+})
+
 app.all('*', (req, res) => {
-    res.send('<h1>This is the homepage</h1>');
+    res.send('<h1>Page not found</h1>');
 })
 
 app.listen(3000, () => {
     console.log('The server is listening on port 3000');
 });
-
