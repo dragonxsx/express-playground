5786ef52cd4c85eefbe9eeb164d186f4f507d687
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-11-05 17:11:14 +0700

    MovieFanApp - Adding the request module

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const request = require('request');
+
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  request.get(nowPlayingUrl, (err, response, bodyData) =>  {
+    console.log('============Error============');
+    console.log(err);
+    console.log('============Body============');
+    console.log(bodyData);
+    console.log('============Request============');
+    console.log(res);
+
+    res.json(JSON.parse(bodyData));
+  });
+
+ 
+  // res.render('index', {});
 });
 
 module.exports = router;
