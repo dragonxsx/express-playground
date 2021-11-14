5e301fcdcae5057701e10d9a0151b31059d1e172
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-11-14 10:40:23 +0700

    MovieFanApi - Change api port

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -2,9 +2,9 @@ const { response } = require('express');
 const express = require('express');
 const request = require('request');
 
-const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
-const apiBaseUrl = 'http://api.themoviedb.org/3';
-const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const apiKey = '123456';
+const apiBaseUrl = 'http://localhost:3030';
+const nowPlayingUrl = `${apiBaseUrl}/movie/most_popular?api_key=${apiKey}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 const router = express.Router();
