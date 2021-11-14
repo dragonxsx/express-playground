60e5f8a260b5c45fb1ee4e49f964363d14fdbbc9
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-11-14 10:43:19 +0700

    MovieFanApi - Adding most_popular route

--- movieFanApi/routes/movie.js
+++ movieFanApi/routes/movie.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const movies = require('../data/movies');
+
 /* GET movie page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Movie' });
+});
+
+router.get('/most_popular', function(req, res, next) {
+  const api_key = req.query.api_key;
+  if(api_key != 123456) {
+    return res.json("Invalid api_key");
+  }
+  
+  let page = req.query.page;
+  if(page === undefined) page = 1;
+
+  let result = movies.filter(movie => movie.most_popular);
+  let startIndex = (page - 1) * 10;
+  result = result.slice(startIndex, startIndex + 9);
+
+  res.json({
+    page,
+    results: result
+  });
 });
 
 module.exports = router;
