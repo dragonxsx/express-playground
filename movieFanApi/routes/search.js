c23a190bf291feee9b8e2f10cf2238ead75fc841
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-11-15 12:34:04 +0700

    MovieFanApi - Adding search routes and using router.use()

--- movieFanApi/routes/search.js
+++ movieFanApi/routes/search.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+const movies = require('../data/movies');
+const people = require('../data/people');
+
+function queryRequired (req, res, next) {
+ const searchTerm = req.query.query;
+ if(!searchTerm) {
+   return res.json({msg: "Query is required"});
+ }
+ next();
+}
+
+router.use(queryRequired);
+
 /* GET search page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Search' });
+});
+
+// GET /movie
+router.get('/movie', function(req, res, next){
+  const searchTerm = req.query.query;
+  const results = movies.filter(movie => {
+    return movie.title.includes(searchTerm) || movie.overview.includes(searchTerm);
+  });
+
+  res.json({results});
+});
+
+// GET /people
+router.get('/person', function(req, res, next){
+  const searchTerm = req.query.query;
+  const results = people.filter(person => {
+    return person.name.includes(searchTerm);
+  });
+
+  res.json({results});
 });
 
 module.exports = router;
