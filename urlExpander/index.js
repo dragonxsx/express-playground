c6c9575d469e39970b9d05485c8e12e4fdf038b5
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-11-29 17:45:22 +0700

    UrlExpander - Expand the short url

--- urlExpander/index.js
+++ urlExpander/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const request = require('request');
 
 const app = express();
 
@@ -8,7 +9,24 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname, 'index.html');
-})
+});
+
+app.get('/expand', (req, res) => {
+    let shortUrl = req.query.shortUrl;
+
+    if(!shortUrl.startsWith('http')) shortUrl = 'https://' + shortUrl;
+    request({
+        url: shortUrl,
+        method: 'HEAD',
+        followAllRedirects: true
+    }, (err, response, body) => {
+        if(err) { 
+            return res.status(400).send('Error');
+        }
+        
+        res.send(response.request.href);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- urlExpander/public/main.js
+++ urlExpander/public/main.js
@@ -3,16 +3,26 @@ const expandBtn = document.querySelector('#expand-btn')
 const result = document.querySelector('#result')
 
 expandBtn.addEventListener('click', () => {
+    result.removeAttribute('href');
+    result.removeAttribute('target');
+    
     // Initally set the result to loading
     result.innerText = 'Loading ...'
-    fetch(`/expand`)
+    
+    fetch(`/expand?shortUrl=${input.value}`)
         .then(res => res.text())
         .then(text => {
+            if(text === 'Error') {
+                result.innerText = 'Invalid input';
+                return;
+            }
+
             // Display the result send from the server
             result.innerText = text
+            result.setAttribute('href', text);
+            result.setAttribute('target', "_blank");
         })
         .catch(err => {
-            console.log(err)
             result.innerText = 'Error'
         })
 })
